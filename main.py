1617ef1bb8142418c162ee605fade8b96cdf4868
Kirill <verikrill@mail.ru>
2025-04-05 09:32:04 +0300

    что то со скоростью надо делать

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ model.generation_config = GenerationConfig.from_pretrained(model_name)
 model.generation_config.pad_token_id = model.generation_config.eos_token_id
 
 messages = [
-    {"role": "user", "content": "Who are you?"}
+    {"role": "user", "content": "Расскажи что такое конституция"}
 ]
 input_tensor = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt")
-outputs = model.generate(input_tensor.to(model.device), max_new_tokens=100)
+outputs = model.generate(input_tensor.to(model.device), max_new_tokens=500)
 
 result = tokenizer.decode(outputs[0][input_tensor.shape[1]:], skip_special_tokens=True)
 print(result)
